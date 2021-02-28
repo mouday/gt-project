ed67a6e7e4febfd6cdb28ea3745443cbccac4075
pengshiyu <1940607002@qq.com>
2021-02-28 15:20:39 +0800

    add

---
+++ templates/template-pyenv.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+################################################
+#  通过国内镜像安装pyenv
+#  copy form https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer
+#  github: https://github.com/pyenv/pyenv-installer
+# 
+#  update: 2021-02-28
+#  author: Peng Shiyu
+################################################
+
+set -e
+[ -n "$PYENV_DEBUG" ] && set -x
+
+if [ -z "$PYENV_ROOT" ]; then
+  PYENV_ROOT="${HOME}/.pyenv"
+fi
+
+colorize() {
+  if [ -t 1 ]; then printf "\e[%sm%s\e[m" "$1" "$2"
+  else echo -n "$2"
+  fi
+}
+
+# Checks for `.pyenv` file, and suggests to remove it for installing
+if [ -d "${PYENV_ROOT}" ]; then
+  { echo
+    colorize 1 "WARNING"
+    echo ": Can not proceed with installation. Kindly remove the '${PYENV_ROOT}' directory first."
+    echo
+  } >&2
+    exit 1
+fi
+
+shell="$1"
+if [ -z "$shell" ]; then
+  shell="$(ps c -p "$PPID" -o 'ucomm=' 2>/dev/null || true)"
+  shell="${shell##-}"
+  shell="${shell%% *}"
+  shell="$(basename "${shell:-$SHELL}")"
+fi
+
+failed_checkout() {
+  echo "Failed to git clone $1"
+  exit -1
+}
+
+checkout() {
+  [ -d "$2" ] || git clone --depth 1 "$1" "$2" || failed_checkout "$1"
+}
+
+if ! command -v git 1>/dev/null 2>&1; then
+  echo "pyenv: Git is not installed, can't continue." >&2
+  exit 1
+fi
+
+if [ -n "${USE_GIT_URI}" ]; then
+#   GITHUB="git://github.com"
+  GITHUB="git://gitee.com"
+else
+#   GITHUB="https://github.com"
+  GITHUB="https://gitee.com/"
+fi
+
+# [github]pyenv -> [gitee]mouday
+checkout "${GITHUB}/mouday/pyenv.git"            "${PYENV_ROOT}"
+checkout "${GITHUB}/mouday/pyenv-doctor.git"     "${PYENV_ROOT}/plugins/pyenv-doctor"
+checkout "${GITHUB}/mouday/pyenv-installer.git"  "${PYENV_ROOT}/plugins/pyenv-installer"
+checkout "${GITHUB}/mouday/pyenv-update.git"     "${PYENV_ROOT}/plugins/pyenv-update"
+checkout "${GITHUB}/mouday/pyenv-virtualenv.git" "${PYENV_ROOT}/plugins/pyenv-virtualenv"
+checkout "${GITHUB}/mouday/pyenv-which-ext.git"  "${PYENV_ROOT}/plugins/pyenv-which-ext"
+
+if ! command -v pyenv 1>/dev/null; then
+  { echo
+    colorize 1 "WARNING"
+    echo ": seems you still have not added 'pyenv' to the load path."
+    echo
+  } >&2
+
+  case "$shell" in
+  bash )
+    profile="~/.bashrc"
+    ;;
+  zsh )
+    profile="~/.zshrc"
+    ;;
+  ksh )
+    profile="~/.profile"
+    ;;
+  fish )
+    profile="~/.config/fish/config.fish"
+    ;;
+  * )
+    profile="your profile"
+    ;;
+  esac
+
+  { echo "# Load pyenv automatically by adding"
+    echo "# the following to ${profile}:"
+    echo
+    case "$shell" in
+    fish )
+      echo "set -x PATH \"${PYENV_ROOT}/bin\" \$PATH"
+      echo 'status --is-interactive; and . (pyenv init -|psub)'
+      echo 'status --is-interactive; and . (pyenv virtualenv-init -|psub)'
+      ;;
+    * )
+      echo "export PATH=\"${PYENV_ROOT}/bin:\$PATH\""
+      echo "eval \"\$(pyenv init -)\""
+      echo "eval \"\$(pyenv virtualenv-init -)\""
+      ;;
+    esac
+  } >&2
+fi
