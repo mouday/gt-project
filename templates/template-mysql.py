5032d80aa67e319ceab7b0c2e923beb11b870c66
pengshiyu <1940607002@qq.com>
2021-03-03 18:13:52 +0800

    fix

--- templates/template-mysql.py
+++ templates/template-mysql.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 # pip install mysql-connector-python
-from mysql.connector import MySQLConnection
+# sql: %s or %(name)s
+from mysql.connector import Connect
 
 
+# doc: https://dev.mysql.com/doc/connector-python/en/connector-python-connectargs.html
 config = {
     'host': 'localhost',
     'port': 3306,
@@ -12,12 +14,7 @@ config = {
     'database': 'data',
 }
 
-
-class CustomMySQLConnection(MySQLConnection):
-    # 默认以字典的形式返回
-    def cursor(self, buffered=None, raw=None, prepared=None, cursor_class=None, dictionary=True, named_tuple=None):
-        return super().cursor(buffered, raw, prepared, cursor_class, dictionary, named_tuple)
-
-
 # 连接数据库获取游标
-db = CustomMySQLConnection(**config)
+db = Connect(**config)
+
+# cursor = db.cursor(dictionary=True)
